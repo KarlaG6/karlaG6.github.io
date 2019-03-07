bc5ba7b1a61a51e443afcf25e09141b118b40422
Karla <karlaisabelgt@gmail.com>
2019-03-07 01:26:41 -0500

    some changes

--- scripts/calBin.js
+++ scripts/calBin.js
@@ -1,3 +1,21 @@
-function init() {
-    var result = document
+function Init (){
+    var operation = document.getElementsByClassName("item0");
+    var atras = document.getElementsByClassName("item1");
+    var alante = document.getElementsByClassName("item2");
+    var Ans = document.getElementsByClassName("item3");
+    var brackets = document.getElementsByClassName("item4");
+    var Delete = document.getElementsByClassName("item5");
+    var reset = document.getElementsByClassName("item6");
+    var suma = document.getElementsByClassName("item7");
+    var mult = document.getElementsByClassName("item8");
+    var one = document.getElementsByClassName("item9");
+    var zero = document.getElementsByClassName("item10");
+    var rest = document.getElementsByClassName("item11");
+    var divid = document.getElementsByClassName("item12");
+    var point = document.getElementsByClassName("item13");
+    var result = document.getElementsByClassName("item14");
+
+    one.onclick = function (event){alert("ghfjdsk")}
 }
+
+
