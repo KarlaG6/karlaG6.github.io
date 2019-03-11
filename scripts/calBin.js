ca81439838c08a7dc6756c88cc7e25f5f7720ed4
Karla <karlaisabelgt@gmail.com>
2019-03-11 14:26:51 -0500

    yisua help

--- scripts/calBin.js
+++ scripts/calBin.js
@@ -1,23 +1,25 @@
-function Init (){
-    var operation = document.getElementsByClassName("item0");
-    var atras = document.getElementsByClassName("item1");
-    var alante = document.getElementsByClassName("item2");
-    var Ans = document.getElementsByClassName("item3");
-    var brackets = document.getElementsByClassName("item4");
-    var Delete = document.getElementsById("delete1");
-    var reset = document.getElementsByClassName("item6");
-    var suma = document.getElementsByClassName("item7");
-    var mult = document.getElementsByClassName("item8");
-    var one = document.getElementsByClassName("item9");
-    var zero = document.getElementsByClassName("item10");
-    var rest = document.getElementsByClassName("item11");
-    var divid = document.getElementsByClassName("item12");
-    var point = document.getElementsByClassName("item13");
-    var result = document.getElementsByClassName("item14");
 
-    zero.onclick =  zero();
-    function zero(){alert("holi")};
-    Delete.addEventListener("click", zero());
-}
+    var operation = document.getElementsById("item0");
+    var atras = document.getElementsById("item1");
+    var alante = document.getElementsById("item2");
+    var Ans = document.getElementsById("item3");
+    var brackets = document.getElementsById("item4");
+    var Delete = document.getElementsById("delete1");
+    var reset = document.getElementsById("item6");
+    var suma = document.getElementsById("item7");
+    var mult = document.getElementsById("item8");
+    var one = document.getElementsById("item9");
+    var zero = document.getElementById("zero");
+    var rest = document.getElementsById("item11");
+    var divid = document.getElementsById("item12");
+    var point = document.getElementsById("item13");
+    var result = document.getElementsById("item14");
+
+
+    /*console.log(zero);
+  zero.addEventListener('click', function(){
+     //screen.value="0"
+  })
+  */
 
 
