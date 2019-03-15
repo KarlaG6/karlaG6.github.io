f8b02d9e929ef7c85995154a0cdfbfdcbc3a2825
Karla <karlaisabelgt@gmail.com>
2019-03-15 13:50:09 -0500

    sirve suma grotescamente

--- scripts/calBin.js
+++ scripts/calBin.js
@@ -14,53 +14,96 @@ var point = document.getElementById("item13");
 var result = document.getElementById("item14");
 var screen = document.getElementById("screen");
 var opA= []; var opB = []; var num = 0; var type = 9;
+var opType =0; var num2=0; var resultOp =0; var resultFin=0;
 
-zero.addEventListener('click',function (){
-    opA.push("0");
-    resultShow.value= opA.join("");
-    num= Number(opA.join("").toString());
-})
-one.addEventListener('click',function (){
-    opA.push("1");
-    resultShow.value= opA.join("");
-    num= Number(opA.join("").toString());
-})
-sum.addEventListener('click',function (){
-    screen.value=num+' + '; 
-    type = 0;
-})
-rest.addEventListener('click',function (){
-    screen.value=num+' - ';
-    type= 1;
-})
-mult.addEventListener('click',function (){
-    screen.value=num+' x '
-    type= 2;
-})
-divid.addEventListener('click',function (){
-    screen.value=num+' / ';
-    type= 3;
-})
+zero.addEventListener('click',zerofnc)
+one.addEventListener('click',onefnc)
+sum.addEventListener('click',sumar)
+rest.addEventListener('click',restar)
+mult.addEventListener('click',mult)
+divid.addEventListener('click',divide)
 point.addEventListener('click',function (){
     screen.value+='.';
 })
 brackets.addEventListener('click',function (){
     //screen.value+='()'
 })
-Delete.addEventListener('click',function (){
-    screen.value=''
+reset.addEventListener('click',function (){
+    screen.value='';
+    resultShow.value="";
 })
-/*function operation(type) {
-    swictch (type){
-        case 0:
-
-            break;
-        case 1:
-
-        break;
-
+result.addEventListener('click',function(){ 
+    resultOp=num+num2;
+    resultFin=resultOp.toString();
+    resultShow.value=resultFin;
+})
+function onefnc(){  
+    if( opType==0 ){
+        opA.push("1");
+        resultShow.value= opA.join("");
+        num= Number(opA.join("").toString());
+    }else {
+        opB.push("1");
+        resultShow.value= opB.join("");
+        num2= Number(opB.join("").toString());
     }
-}*/
+    
+}
+function zerofnc(){
+    if(opType==0){
+        opA.push("0");
+        resultShow.value= opA.join("");
+        num= Number(opA.join("").toString());
+    }else {
+        opB.push("0");
+        resultShow.value= opB.join("");
+        num2= Number(opB.join("").toString());
+    }
+    
+}
+function sumar(){
+    screen.value=num+' + '; 
+    type = 0;
+    opType=1;
+    
+}
+function restar(){
+    screen.value=num+' - ';
+    type= 1;
+    opType=1
+}
+function mult(){
+    screen.value=num+' x ';
+    type= 2;
+    opType=1
+}
+function divide(){
+    screen.value=num+' / ';
+    type= 3;
+    opType=1
+}
+function operations(){
+    switch(type){
+        case 0:
+            console.log("suma");
+            //resultFin=resultOp.toString();
+            break;
+
+        case 1:
+            console.log("resta");
+            break;
+
+        case 2:
+            console.log("mult");
+            break;
+
+        case 3:
+            console.log("divid");
+            break;
+ }
+}
+    
+
 
 
 
