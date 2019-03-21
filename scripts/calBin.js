32cc47bf89fdf4863ef9df4b374049a359c45ae2
Karla <karlaisabelgt@gmail.com>
2019-03-21 10:58:51 -0500

    Hasta ahora operaciones decimales, bien

--- scripts/calBin.js
+++ scripts/calBin.js
@@ -5,7 +5,7 @@ var brackets = document.getElementById("item4");
 var Delete = document.getElementById("delete1");
 var reset = document.getElementById("item6");
 var sum = document.getElementById("item7");
-var mult = document.getElementById("item8");
+var multi = document.getElementById("item8");
 var one = document.getElementById("item9");
 var zero = document.getElementById("zero");
 var rest = document.getElementById("item11");
@@ -16,27 +16,46 @@ var screen = document.getElementById("screen");
 var opA= []; var opB = []; var num = 0; var type = 9;
 var opType =0; var num2=0; var resultOp =0; var resultFin=0;
 
-zero.addEventListener('click',zerofnc)
-one.addEventListener('click',onefnc)
-sum.addEventListener('click',sumar)
-rest.addEventListener('click',restar)
-mult.addEventListener('click',mult)
-divid.addEventListener('click',divide)
+zero.addEventListener('click',zerofnc);
+one.addEventListener('click',onefnc);
+sum.addEventListener('click',sumar);
+rest.addEventListener('click',restar);
+multi.addEventListener('click',mult);
+divid.addEventListener('click',divide);
 point.addEventListener('click',function (){
     screen.value+='.';
-})
+});
 brackets.addEventListener('click',function (){
     //screen.value+='()'
-})
+});
 reset.addEventListener('click',function (){
     screen.value='';
     resultShow.value="";
-})
+    
+});
 result.addEventListener('click',function(){ 
-    resultOp=num+num2;
+    switch(type){
+        case 0:
+            resultOp=num+num2;
+            break;
+
+        case 1:
+            resultOp=num-num2;
+            break;
+
+        case 2:
+            resultOp=(num)*(num2);
+            console.log("mult");
+            break;
+
+        case 3:
+            resultOp=num/(num2);
+            break;
+    }
+
     resultFin=resultOp.toString();
     resultShow.value=resultFin;
-})
+});
 function onefnc(){  
     if( opType==0 ){
         opA.push("1");
@@ -70,38 +89,19 @@ function sumar(){
 function restar(){
     screen.value=num+' - ';
     type= 1;
-    opType=1
+    opType=1;
 }
 function mult(){
     screen.value=num+' x ';
     type= 2;
-    opType=1
+    opType=1;
 }
 function divide(){
     screen.value=num+' / ';
     type= 3;
-    opType=1
+    opType=1;
 }
-function operations(){
-    switch(type){
-        case 0:
-            console.log("suma");
-            //resultFin=resultOp.toString();
-            break;
 
-        case 1:
-            console.log("resta");
-            break;
-
-        case 2:
-            console.log("mult");
-            break;
-
-        case 3:
-            console.log("divid");
-            break;
- }
-}
     
 
 
